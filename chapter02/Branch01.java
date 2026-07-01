ee6cd753cd1a9ee5db29760a8c2c91b0c343cc1e
yeom <yeomsh03@gmail.com>
2026-07-01 10:04:32 +0900

    version4_g

--- chapter02/Branch01.java
+++ chapter02/Branch01.java
@@ -3,7 +3,7 @@ package chapter02;
 public class Branch01 {
 
     public Branch01(){
-        System.out.println("메인 브렌치에서 작업");
+        System.out.println("구글 브렌치에서 작업");
         System.out.println("다음 브렌치에서 작업");
     }
     
